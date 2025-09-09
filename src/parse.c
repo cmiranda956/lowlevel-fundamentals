659d7b3ecfaaf2a000795759502b9caaf8128d87
typhon <cmiranda956@gmail.com>
2025-09-09 14:42:23 -0500

    fixed create_db_header. too many args in prototype

--- src/parse.c
+++ src/parse.c
@@ -77,7 +77,7 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
 }
 
 // returns a status and not a value
-int create_db_header(int fd, struct dbheader_t **headerOut) {
+int create_db_header(struct dbheader_t **headerOut) {
     struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
     if(header == -1) {
         printf("calloc failed to create dbheader.\n");
