3b7d58dc247a4033b18151ed6d6c9214fd0f0161
typhon <cmiranda956@gmail.com>
2025-09-09 20:39:51 -0500

    testing for null string in add_employee. IF THIS WAS IT.....

--- src/parse.c
+++ src/parse.c
@@ -28,6 +28,10 @@ void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
 }
 
 int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees, char *addstring) {
+    if(addstring == NULL) {
+        printf("missing string argument from user");
+        return STATUS_ERROR;
+    }
     char *name = strtok(addstring, ",");
     char *addr= strtok(NULL, ",");
     char *hours = strtok(NULL, ",");
